3ad9fa6355ff4c2895f5c724ba05cd22732ce903
Kamil Lewandowski <klewandowski16@gmail.com>
2019-04-28 14:30:29 +0200

    Dodanie klasy(kolekcji) przechowującej zadania

---
+++ ZadaniaWPF/ZadaniaWPF/Model/Zadania.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ZadaniaWPF.Model
+{
+    public class Zadania : IEnumerable<Zadanie>
+    {
+        private List<Zadanie> listaZadań = new List<Zadanie>();
+
+        public void DodajZadanie(Zadanie zadanie)
+        {
+            listaZadań.Add(zadanie);
+        }
+
+        public bool UsuńZadanie(Zadanie zadanie)
+        {
+            return listaZadań.Remove(zadanie);
+        }
+
+        public int LiczbaZadań
+        {
+            get
+            {
+                return listaZadań.Count;
+            }
+        }
+
+        public Zadanie this[int indeks]
+        {
+            get
+            {
+                return listaZadań[indeks];
+            }
+        }
+
+        public IEnumerator<Zadanie> GetEnumerator()
+        {
+            return listaZadań.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this.GetEnumerator();
+        }
+
+    }
+}
